f879d3cbb1f2cd94dda3581ec89686700ed39af7
tanmaykulkarni2112 <tanmaykulkarni2112@gmail.com>
2025-02-25 00:32:17 +0530

    Project-1 Server

---
+++ go-server/README.md
@@ -0,0 +1,10 @@
+SERVER 
+
+1 `/` -> index.html
+Route route will serve index.html
+
+2 `/hello` -> hello func
+Will just call hello function
+
+3 `/form` -> form func -> form.html
+form will be associated and it will be opened
---
+++ go-server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Printf("Starting server at port 8080 \n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err : %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post request successful")
+	name := r.FormValue("name")
+	Address := r.FormValue("Address")
+	fmt.Fprintf(w, "The name is %s \n", name)
+	fmt.Fprintf(w, "Address is %s \n", Address)
+}
